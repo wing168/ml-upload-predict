44999b4f228a4735af1e0fcfc1a0c81e35d92dee
wing168 <wh.man@btinternet.com>
2021-09-19 12:40:55 +0100

    client: initial UI mock up

---
+++ client/src/components/Table/Table.tsx
@@ -0,0 +1,34 @@
+import './Table.styles.css';
+
+const Table = () => {
+    return (
+        <div className='model-table'>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Model name</th>
+                        <th scope="col">URL</th>
+                        <th scope="col">Size</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>XGBoost</td>
+                        <td>https://ml-predict/XGboost-1</td>
+                        <td>30,256 kB</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>CATBoost</td>
+                        <td>https://ml-predict/CATboost-2</td>
+                        <td>30,256 kB</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table;
